1b8627e465dfaa82be46d07d908f0c307380c0f6
JeanCamargoReal <jeancamargoreal@gmail.com>
2021-01-20 10:57:24 -0300

    Corrigindo progress bar para trabalhar em %

--- EggTimer/ViewController.swift
+++ EggTimer/ViewController.swift
@@ -19,29 +19,36 @@ class ViewController: UIViewController {
     
     let eggTimes = ["Soft": 3, "Medium": 4, "Hard": 7]
 
-    var secondsRemaining = 60
-
     var timer = Timer()
+    
+    var totalTime = 0
+    var secondsPassed = 0
 
 
     @IBAction func hardnessSelected(_ sender: UIButton) {
-        
-        progressBar.progress = 1.0
         
         timer.invalidate()
 
         let hardness = sender.currentTitle!
         
-        secondsRemaining = eggTimes[hardness]!
+        totalTime = eggTimes[hardness]!
+        
+        progressBar.progress = 0.0
+        
+        secondsPassed = 0
+        
+        tittleLabel.text = hardness
         
         timer = Timer.scheduledTimer(timeInterval: 1.0, target: self,
                              selector: #selector(updateTimer), userInfo: nil, repeats: true)
     }
     
     @objc func updateTimer() {
-        if secondsRemaining > 0 {
-            print("\(secondsRemaining) seconds.")
-            secondsRemaining -= 1
+        if secondsPassed < totalTime {
+            secondsPassed += 1
+            progressBar.progress = Float(secondsPassed) / Float(totalTime)
+            
+            
         } else {
             timer.invalidate()
             tittleLabel.text = "DONE!"
